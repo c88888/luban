263822ab2a1e3caefbb582000402417b2e5648cd
bright.ma <bright.ma@magesfc.com>
2023-07-29 12:06:40 +0800

    main.go: update gin.log path
    Change-Id: Ic686272264d261fff7d52dab0c454641ac83ea2c

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ import (
 
 func main() {
 	// 创建记录日志的文件
-	f, _ := os.Create("gin.log")
+	f, _ := os.Create("logs/gin.log")
 	//gin.DefaultWriter = io.MultiWriter(f)
 	// 如果需要将日志同时写入文件和控制台，请使用以下代码
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
